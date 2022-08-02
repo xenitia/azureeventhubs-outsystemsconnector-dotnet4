befcef5588e04caf6ad13f5a67860159a9d40ad8
Minas Casiou <minascasiou@ingenii-fin.com.au>
2022-08-02 18:30:36 +1000

    Add project files.

---
+++ EventHubReceiver/Receive.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Azure.Messaging.EventHubs;
+using Azure.Messaging.EventHubs.Consumer;
+using Azure.Messaging.EventHubs.Processor;
+using Azure.Storage.Blobs;
+using System.Collections.Concurrent;
+using System.Diagnostics;
+using Azure.Messaging.EventHubs.Primitives;
+using Azure.Security.KeyVault.Secrets;
+using Azure.Identity;
+using System.Threading;
+
+namespace AzureEventHubs.OutSystemsConnector.DotNet4
+{
+    internal class EventHubReceiver
+    {
+
+        static SecretClient client = new SecretClient(vaultUri: new Uri("https://eventhubsnskeyvault.vault.azure.net/"), credential: new DefaultAzureCredential());
+
+        static KeyVaultSecret secret;
+        static String eventHubsConnectionString;
+        static String storageConnectionString;
+
+        static String blobContainerName = "eventhubblob";
+
+        static String eventHubName = "eventhubtopic1";
+        static String consumerGroup = EventHubConsumerClient.DefaultConsumerGroupName;
+        static int maximumBatchSize = 1000;
+
+
+        async static Task Main(string[] args)
+        {
+
+            secret = client.GetSecret("eventHubsConnectionString");
+            eventHubsConnectionString = secret.Value;
+            secret = client.GetSecret("storageConnectionString");
+            storageConnectionString = secret.Value;
+
+            BlobContainerClient storageClient = new BlobContainerClient(storageConnectionString, blobContainerName);
+
+            var processor = new MFCustomEventHubProcessor(
+                storageClient,
+                maximumBatchSize,
+                consumerGroup,
+                eventHubsConnectionString,
+                eventHubName);
+
+            CancellationTokenSource cancellationSource = new CancellationTokenSource();
+            cancellationSource.CancelAfter(TimeSpan.FromSeconds(30));
+
+            // Starting the processor does not block when starting; delay
+            // until the cancellation token is signaled.
+
+            try
+            {
+                await processor.StartProcessingAsync(cancellationSource.Token);
+                await Task.Delay(Timeout.Infinite, cancellationSource.Token);
+            }
+            catch (TaskCanceledException)
+            {
+                // This is expected if the cancellation token is
+                // signaled.
+            }
+            finally
+            {
+                // Stopping may take up to the length of time defined
+                // as the TryTimeout configured for the processor;
+                // By default, this is 60 seconds.
+
+                await processor.StopProcessingAsync();
+            }
+        }
+    }
+}
---
+++ EventHubSender/Send.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Azure.Identity;
+using Azure.Messaging.EventHubs;
+using Azure.Messaging.EventHubs.Producer;
+using Azure.Security.KeyVault.Secrets;
+
+namespace AzureEventHubs.OutSystemsConnector.DotNet4
+{
+    internal class Send
+    {
+        static SecretClient client = new SecretClient(vaultUri: new Uri("https://eventhubsnskeyvault.vault.azure.net/"), credential: new DefaultAzureCredential());
+
+        static String eventHubsConnectionString;
+        static String eventHubName = "eventhubtopic1";
+        static int batchSize = 1000; //1, 10, 1000, 5000
+        static string ComputerName = System.Net.Dns.GetHostName();
+
+        async static Task Main(string[] args)
+        {
+            KeyVaultSecret secret = client.GetSecret("eventHubsConnectionString");
+            eventHubsConnectionString = secret.Value;
+            ComputerName = System.Net.Dns.GetHostName();
+
+            await sendMicroBatchPartitionKey();
+        }
+
+        async static Task sendMicroBatchPartitionKey()
+        {
+            var producer = new EventHubProducerClient(eventHubsConnectionString, eventHubName);
+
+            try
+            {
+                var batchOptions = new CreateBatchOptions
+                {
+                    PartitionKey = "ABCDE"
+                };
+
+                //string firstPartition = (await producer.GetPartitionIdsAsync()).First();
+
+                //var batchOptions = new CreateBatchOptions
+                //{
+                //    PartitionId = firstPartition
+                //};
+
+                for (var counter = 0; counter < int.MaxValue; ++counter)
+                //for (var counter = 0; counter < 5; ++counter)
+
+                {
+                    //producer = new EventHubProducerClient(connectionString, eventHubName);
+                    EventDataBatch eventBatch = await producer.CreateBatchAsync(batchOptions);
+                    var eventData = new EventData();
+
+                    for (var index = 0; index < batchSize; ++index)
+                    {
+                        //eventData = new EventData($"PartitionId:{firstPartition} Host:{ComputerName}Event #{counter}:{index} Id:{Guid.NewGuid()} Timestamp:{DateTime.Now.ToString()}");
+                        eventData = new EventData($"PartitionKey:{batchOptions.PartitionKey} Host:{ComputerName}Event #{counter}:{index} Id:{Guid.NewGuid()} Timestamp:{DateTime.Now.ToString()}");
+                        eventData.MessageId = Guid.NewGuid().ToString();
+                        eventData.CorrelationId = Guid.NewGuid().ToString();
+
+                        Console.WriteLine($"PRE: Adding to Micro Batch Payload: Event to partitionKey: {batchOptions.PartitionKey} with length {eventData.EventBody.ToArray().Length}, eventBody = {eventData.EventBody}.");
+
+                        if (!eventBatch.TryAdd(eventData))
+                        {
+                            //throw new Exception($"The event at Host:{ComputerName} { index } could not be added.");
+                            break;
+                        }
+                        //Thread.Sleep(10);
+
+                    }
+                    //Thread.Sleep(3000);
+                    await producer.SendAsync(eventBatch);
+                    //Thread.Sleep(3000);
+                    Console.WriteLine($"POST: Sent Micro Batch Payload."); // Event to partitionKey: {batchOptions.PartitionKey} with length {eventData.EventBody.ToArray().Length}, eventBody = {eventData.EventBody}.");
+
+                    //await producer.CloseAsync();
+                    //Thread.Sleep(1000);
+                }
+                //await producer.SendAsync(eventBatch);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                // Transient failures will be automatically retried as part of the
+                // operation. If this block is invoked, then the exception was either
+                // fatal or all retries were exhausted without a successful publish.
+            }
+            finally
+            {
+                await producer.CloseAsync();
+            }
+        }
+    }
+
+}
