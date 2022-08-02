250449e70ff37ee98ed63dc6dfdb98841d111dfa
Minas Casiou <minascasiou@ingenii-fin.com.au>
2022-08-02 23:32:46 +1000

    OutSystems Extension

---
+++ AzureEventHubsSenderExtension2/Source/NET/AzureEventHubsSenderExtension2.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Data;
+using OutSystems.HubEdition.RuntimePlatform;
+using OutSystems.RuntimePublic.Db;
+
+namespace OutSystems.NssAzureEventHubsSenderExtension2 {
+
+	public class CssAzureEventHubsSenderExtension2: IssAzureEventHubsSenderExtension2 {
+
+		/// <summary>
+		/// Import Details:
+		/// 
+		///  - Action SenderSend:
+		///         Target: Method Send(string, out string)
+		///         Declaring Type: ClassLibraryOSTest.Sender
+		///         Parameters:
+		///             msgIn(Text &lt;- string)
+		///             msgOut(Text &lt;- string)
+		/// </summary>
+		/// <param name="ssmsgIn"></param>
+		/// <param name="ssmsgOut"></param>
+		public void MssSenderSend(string ssmsgIn, out string ssmsgOut) {
+			ssmsgOut = "";
+			// Implementation for action
+			ClassLibraryOSTest.Sender.Send(ssmsgIn, out ssmsgOut);
+		} // MssSenderSend
+
+
+	} // CssAzureEventHubsSenderExtension2
+
+} // OutSystems.NssAzureEventHubsSenderExtension2
+
