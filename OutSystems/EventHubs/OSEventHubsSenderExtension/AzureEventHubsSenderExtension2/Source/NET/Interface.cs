90de8800ddf90695c91982d21cb3fec254b4e541
Minas Casiou <minascasiou@ingenii-fin.com.au>
2022-08-09 01:32:23 +1000

    Updated OutSystems Extension Artefacts

---
+++ OutSystems/EventHubs/OSEventHubsSenderExtension/AzureEventHubsSenderExtension2/Source/NET/Interface.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Data;
+using OutSystems.HubEdition.RuntimePlatform;
+
+namespace OutSystems.NssAzureEventHubsSenderExtension2 {
+
+	public interface IssAzureEventHubsSenderExtension2 {
+
+		/// <summary>
+		/// Import Details:
+		/// 
+		///  - Action SenderSend:
+		///         Target: Method Send(string, out string)
+		///         Declaring Type: ClassLibraryOSTest.Sender
+		///         Parameters:
+		///             msgIn(Text &lt;- string)
+		///             msgOut(Text &lt;- string)
+		/// </summary>
+		/// <param name="ssmsgIn"></param>
+		/// <param name="ssmsgOut"></param>
+		void MssSenderSend(string ssmsgIn, out string ssmsgOut);
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="ssBusinessEvent"></param>
+		/// <param name="ssmsgIn1"></param>
+		/// <param name="ssmsgOut"></param>
+		void MssSenderSend3(string ssBusinessEvent, string ssmsgIn1, out string ssmsgOut);
+
+	} // IssAzureEventHubsSenderExtension2
+
+} // OutSystems.NssAzureEventHubsSenderExtension2
---
+++ OutSystems/EventHubs/OSEventHubsSenderExtension/AzureEventHubsSenderExtension2/Templates/NET/AzureEventHubsSenderExtension2.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using System.Data;
+using OutSystems.HubEdition.RuntimePlatform;
+using OutSystems.RuntimePublic.Db;
+
+namespace OutSystems.NssAzureEventHubsSenderExtension2 {
+
+	public class CssAzureEventHubsSenderExtension2: IssAzureEventHubsSenderExtension2 {
+
+		/// <summary>
+		/// Import Details:
+		/// 
+		///  - Action SenderSend:
+		///         Target: Method Send(string, out string)
+		///         Declaring Type: ClassLibraryOSTest.Sender
+		///         Parameters:
+		///             msgIn(Text &lt;- string)
+		///             msgOut(Text &lt;- string)
+		/// </summary>
+		/// <param name="ssmsgIn"></param>
+		/// <param name="ssmsgOut"></param>
+		public void MssSenderSend(string ssmsgIn, out string ssmsgOut) {
+			ssmsgOut = "";
+			// Implementation for action
+			ClassLibraryOSTest.Sender.Send(ssmsgIn, out ssmsgOut);
+		} // MssSenderSend
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="ssBusinessEvent"></param>
+		/// <param name="ssmsgIn1"></param>
+		/// <param name="ssmsgOut"></param>
+		public void MssSenderSend3(string ssBusinessEvent, string ssmsgIn1, out string ssmsgOut) {
+			ssmsgOut = "";
+			// TODO: Write implementation for action
+		} // MssSenderSend3
+
+	} // CssAzureEventHubsSenderExtension2
+
+} // OutSystems.NssAzureEventHubsSenderExtension2
+
